f6d0679ef3668eebad7947f202d4e7eac49fd80b
raicem <cem.unalan@automattic.com>
2026-03-25 01:24:28 +0300

    Persist last directory in fish and update git config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,15 @@
 # Disable fish greeting
 set -U fish_greeting
 
+# Start in last directory
+if set -q __fish_last_dir
+    cd $__fish_last_dir
+end
+
+function __fish_save_last_dir --on-variable PWD
+    set -gU __fish_last_dir $PWD
+end
+
 # Environment variables
 set -x EDITOR nvim
 set -x VISUAL nvim
@@ -12,7 +21,7 @@ alias grep="rg"
 alias clip="xclip -selection clipboard -i"
 
 # opencode
-fish_add_path /home/raicem/.opencode/bin
+fish_add_path $HOME/.opencode/bin
 
 # Tool initializations
 if test -x ~/.local/bin/mise
